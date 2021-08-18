d3f389e435e62ebb3ba355f609e509c29be62e46
git的用户名 <git>
2021-08-18 14:08:59 +0800

    mpvue代码提交

---
+++ src/pages/api/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>api页面</div>
+</template>
+<script>
+export default {
+  beforeCreate() {
+    console.log("1,beforeCreate");
+  },
+  created() {
+    console.log("2,created");
+  },
+  onLoad() {
+    console.log("3,onLoad监听页面加载");
+  },
+  onShow() {
+    console.log("4,onShow");
+  },
+  onReady() {
+    console.log("5,onReady监听页面初次渲染完成");
+  },
+  beforeMount() {
+    console.log("6,beforeMount");
+  },
+  mounted() {
+    console.log("7,mounted");
+  },
+};
+</script>
---
+++ src/pages/components/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container gray">
+    <ul class="clearfix">
+      <li>
+        基础组件
+        <ul>
+          <li>scroll-view</li>
+          <li>swiper、swiper-item</li>
+          <li>view</li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  beforeCreate() {
+    console.log("1,beforeCreate");
+  },
+  created() {
+    console.log("2,created");
+  },
+  onLoad() {
+    console.log("3,onLoad监听页面加载");
+  },
+  onShow() {
+    console.log("4,onShow");
+  },
+  onReady() {
+    console.log("5,onReady监听页面初次渲染完成");
+  },
+  beforeMount() {
+    console.log("6,beforeMount");
+  },
+  mounted() {
+    console.log("7,mounted");
+  },
+};
+</script>
+<style scoped lang="scss">
+ul > li {
+  line-height: 1.5;
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--border-color);
+  }
+}
+</style>
